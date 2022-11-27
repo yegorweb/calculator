f00e6989c1be9a3c34f81b1b62bbe66cbc1dcd6c
Yegor <egorknyazev10@gmail.com>
2022-11-27 17:46:05 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { appWindow, LogicalSize, PhysicalSize } from "@tauri-apps/api/window";
+
+appWindow.setTitle('Калькулятор')
+appWindow.setIcon("D:/egor/Site/Learn/calculator/src/assets/logo.png")
+appWindow.setMinSize(new PhysicalSize(320, 500))
+appWindow.setSize(new LogicalSize(600, 500))
+</script>
+
+<template>
+	<div class="container">calculator</div>
+</template>
+
+<style lang="scss">
+@import '@/assets/style.scss';
+</style>
