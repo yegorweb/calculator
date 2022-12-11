2ceff50ab9ee945cb99d0b7c9812ff4c399ecede
Yegor Knyazev <egorknyazev10@gmail.com>
2022-12-11 17:54:37 +0400

    added enter symbols from keyboard, big bugfix

--- src/components/ButtonsSection.vue
+++ src/components/ButtonsSection.vue
@@ -1,28 +1,28 @@
 <template>
     <div class="buttons-container">
         <div class="section section-other">
-            <Button @click="calculateStore.removeDigit()" color="" background="#FF9E58">←</Button>
+            <Button @click="calculateStore.removeSymbol()" color="" background="#FF9E58">←</Button>
             <Button @click="calculateStore.clear()" color="" background="#FF9E58">C</Button>
             <Button color="" background="">( )</Button>
             <Button color="" background="">x<sup>2</sup></Button>
         </div>
         <div class="section section-main">
-            <Button @click="calculateStore.add_digit('1')" color="" background="">1</Button>
-            <Button @click="calculateStore.add_digit('2')" color="" background="">2</Button>
-            <Button @click="calculateStore.add_digit('3')" color="" background="">3</Button>
-            <Button @click="calculateStore.add_digit('+')" color="" background="">+</Button>
-            <Button @click="calculateStore.add_digit('4')" color="" background="">4</Button>
-            <Button @click="calculateStore.add_digit('5')" color="" background="">5</Button>
-            <Button @click="calculateStore.add_digit('6')" color="" background="">6</Button>
-            <Button @click="calculateStore.add_digit('-')" color="" background="">-</Button>
-            <Button @click="calculateStore.add_digit('7')" color="" background="">7</Button>
-            <Button @click="calculateStore.add_digit('8')" color="" background="">8</Button>
-            <Button @click="calculateStore.add_digit('9')" color="" background="">9</Button>
-            <Button @click="calculateStore.add_digit('×')" color="" background="">×</Button>
-            <Button @click="calculateStore.add_digit('.')" color="" background="">.</Button>
-            <Button @click="calculateStore.add_digit('0')" color="" background="">0</Button>
+            <Button @click="calculateStore.add_symbol('1')" color="" background="">1</Button>
+            <Button @click="calculateStore.add_symbol('2')" color="" background="">2</Button>
+            <Button @click="calculateStore.add_symbol('3')" color="" background="">3</Button>
+            <Button @click="calculateStore.add_symbol('+')" color="" background="">+</Button>
+            <Button @click="calculateStore.add_symbol('4')" color="" background="">4</Button>
+            <Button @click="calculateStore.add_symbol('5')" color="" background="">5</Button>
+            <Button @click="calculateStore.add_symbol('6')" color="" background="">6</Button>
+            <Button @click="calculateStore.add_symbol('-')" color="" background="">-</Button>
+            <Button @click="calculateStore.add_symbol('7')" color="" background="">7</Button>
+            <Button @click="calculateStore.add_symbol('8')" color="" background="">8</Button>
+            <Button @click="calculateStore.add_symbol('9')" color="" background="">9</Button>
+            <Button @click="calculateStore.add_symbol('×')" color="" background="">×</Button>
+            <Button @click="calculateStore.add_symbol('.')" color="" background="">.</Button>
+            <Button @click="calculateStore.add_symbol('0')" color="" background="">0</Button>
             <Button @click="calculateStore.calculate()" color="" background="">=</Button>
-            <Button @click="calculateStore.add_digit('÷')" color="" background="">÷</Button>
+            <Button @click="calculateStore.add_symbol('÷')" color="" background="">÷</Button>
         </div>
     </div>
 </template>
@@ -32,6 +32,29 @@ import { useCalculateStore } from "../stores/calculateStore"
 import Button from "./Button.vue";
 
 var calculateStore = useCalculateStore()
+
+function add_symbol(e: KeyboardEvent): void {
+    console.log(e.key)
+    e.key == '1' ? calculateStore.add_symbol('1'): null
+    e.key == '2' ? calculateStore.add_symbol('2'): null
+    e.key == '3' ? calculateStore.add_symbol('3'): null
+    e.key == '+' ? calculateStore.add_symbol('+'): null
+    e.key == '4' ? calculateStore.add_symbol('4'): null
+    e.key == '5' ? calculateStore.add_symbol('5'): null
+    e.key == '6' ? calculateStore.add_symbol('6'): null
+    e.key == '-' ? calculateStore.add_symbol('-'): null
+    e.key == '7' ? calculateStore.add_symbol('7'): null
+    e.key == '8' ? calculateStore.add_symbol('8'): null
+    e.key == '9' ? calculateStore.add_symbol('9'): null
+    e.key == '*' ? calculateStore.add_symbol('×'): null
+    e.key == '.' ? calculateStore.add_symbol('.'): null
+    e.key == '0' ? calculateStore.add_symbol('0'): null
+    e.key == '/' ? calculateStore.add_symbol('÷'): null
+    e.key == 'c' ? calculateStore.clear(): null    
+    e.code == 'Backspace' ? calculateStore.removeSymbol(): null
+    e.code == 'Enter' ? calculateStore.calculate(): null
+}
+window.addEventListener('keyup', add_symbol)
 </script>
 
 <style lang="scss" scoped>
--- src/stores/calculateStore.ts
+++ src/stores/calculateStore.ts
@@ -1,81 +1,113 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { Ref, ref } from 'vue'
 
 export const useCalculateStore = defineStore('calculateStore', () => {
 
     /*********************** Variables ***********************/
 
-    /** Прошлое обработанное выражение */
-    let past_expression = ref('')
+    /** Прошлое, обработанное выражение */
+    let past_expression: Ref<string> = ref('')
 
     /** Выражение, которое видит юзер */
-    let current_expression = ref('0')
+    let current_expression: Ref<string> = ref('0')
     
-    /** Вычисляемое выражение */
-    let current_expression_computable : string = ''
+    /** Вычисляемое выражение (current_expression, но без ×, ÷ и прочего, чтобы js нормально посчитал) */
+    let current_expression_computable: string = ''
     
     /** Последний вводимый символ */
-    let last_digit : string = ''
+    let last_symbol: string = ''
+
+    /** Получили ошибку? */
+    let got_error: boolean = false
 
     /******************* End of variables ********************/
 
 
 
-    /*********************** Magic ✨ ************************/
+    /*********************** Magic ✨ ***********************/
 
-    /** Это число? Если да, вернет true */
-    function isNumber(str: string): Boolean {
+    /** Параметр - число? Если да, вернет true */
+    function isNumber(str: string): boolean {
         return /^(\d+,)*(\d+)*(\d+.)$/.test(str)
+    }
+
+    /** Параметр - математическое действие? Если да, вернет true 
+     * @param symbol {string} Символ
+    */
+    function isMathAction(symbol: string): boolean {
+        return /[^\d\.\d]/.test(symbol)
     }
 
     /** Возвращает массив всех чисел из текущего выражения */
     function getAllNumbers(): Array<string> {
-        return current_expression.value.split('')///+|-|×|÷|(|)/)
+        return current_expression.value.split(/[^\d\.\d]/) 
+        // Я в ашале. Сам написал регулярное выражение и оно работает 
     }
 
     /** Возвращает последнее число */
-    function getLastNumber(): any {
-        return getAllNumbers().at(-1)
+    function getLastNumber(): string {
+        let result = getAllNumbers()
+        return result[result.length - 1]
     }
 
     /** Добавляет символ */
-    function add_digit(digit: string): any {
+    function add_symbol(entered_symbol: string): void {
+
+        // Удаляет выражение и ставит символ, если в выражении только 0
         if (current_expression.value == '0') {
-            current_expression.value = digit
-            last_digit = digit
+            current_expression.value = entered_symbol
+            last_symbol = entered_symbol
             return
         }
-        if (!isNumber(current_expression.value) && last_digit == digit) return
+        
+        if (!isNumber(current_expression.value) && last_symbol == entered_symbol) return
 
-        last_digit = digit
-        current_expression.value += digit
+        // Не дает поставить математическое действие, если в конце нет числа
+        if (isMathAction(current_expression.value.slice(-1)) && isMathAction(entered_symbol)) return
+        
+        // Не дает поставить точку, если точка уже поставлена
+        if (getLastNumber().split('').some(el => el == '.') && entered_symbol == '.') return
+
+        // Очищаем выражение и ставим символ, если до этого поучили ошибку
+        if (got_error) {
+            current_expression.value = ''
+            got_error = false
+        }
+
+        last_symbol = entered_symbol
+        current_expression.value += entered_symbol
     }
 
     /** Убирает символ */
-    function removeDigit(): any {
+    function removeSymbol(): void {
         current_expression.value.length == 1 ? current_expression.value = '0' : current_expression.value = current_expression.value.slice(0, -1)
     }
 
     /** Очищает выражение */
-    function clear(): any {
+    function clear(): void {
         current_expression.value == '0' ? past_expression.value = '' : null
         current_expression.value = '0'
     }
 
     /** Считает выражение */
-    function calculate(): any {
+    function calculate(): void {
+        console.log(getAllNumbers())
         past_expression.value = current_expression.value
         console.log(past_expression.value)
         try {
             current_expression.value = eval(current_expression.value)
+            got_error = false
         } catch(e) {
             console.error(e)
             current_expression.value = 'Error'
+            got_error = true
         }
     }
 
-    // NOTE: Не забыть закинуть функции сюда
-    return { past_expression, current_expression, add_digit, removeDigit, clear, calculate }
+    /******************** End of magic ✨ ********************/
+
+
+    return { past_expression, current_expression, add_symbol, removeSymbol, clear, calculate }
 })
 
 
